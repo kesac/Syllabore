c46ba65483d78d811491ad80d094f080453c2134
kesac <kevin@turtlesort.com>
2019-12-26 14:42:12 -0700

    Ensured that the example generator XML definition file is equivalent to StandaloneSyllableProvider

--- Syllabore/Syllabore/StandaloneSyllableProvider.cs
+++ Syllabore/Syllabore/StandaloneSyllableProvider.cs
@@ -14,7 +14,7 @@ namespace Syllabore
         private static readonly string[] StartingConsonants = { "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z" };
         private static readonly string[] StartingConsonantSequences = { "CH", "SH", "BL", "CL", "FL", "PL", "GL", "BR", "CR", "DR", "PR", "TR", "TH", "SC", "SP", "ST", "SL", "SPR" };
         private static readonly string[] Vowels = { "A", "E", "I", "O", "U" };
-        private static readonly string[] VowelSequences = { "AE", "EA", "AI", "IA", "AU", "AY", "IE", "OI", "OU", "EY", };
+        private static readonly string[] VowelSequences = { "AE", "EA", "AI", "IA", "AU", "AY", "IE", "OI", "OU", "EY" };
         private static readonly string[] EndingConsonants = { "B", "C", "D", "F", "G", "H", "K", "L", "M", "N", "P", "R", "S", "T", "V", "X", "Y"};
         private static readonly string[] EndingConsonantSequences = { "CK", "ST", "SC", "NG", "NK", "RSH", "LSH", "RK", "RST", "NCT", "XT" };
 
