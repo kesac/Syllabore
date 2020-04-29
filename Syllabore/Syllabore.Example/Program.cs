c741c91e1bdff912d4a481b10cdb2a352d324a00
kesac <kevin@turtlesort.com>
2020-04-28 22:36:04 -0600

    Added example usage of new Shifter classes to produce variations of names

--- Syllabore/Syllabore.Example/Program.cs
+++ Syllabore/Syllabore.Example/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Syllabore.Example
 {
@@ -6,26 +8,56 @@ namespace Syllabore.Example
     {
         public static void Main(string[] args)
         {
+            // Creating a vanilla name generator with a validator
+            // with no configuration or loading of definition files
             var provider = new StandaloneSyllableProvider();
             var validator = new StandaloneNameValidator();
 
             var names = new NameGenerator(provider, validator);
 
-            for (int i = 0; i < 1; i++)
+            for (int i = 0; i < 10; i++)
             {
                 System.Console.WriteLine(names.Next());
             }
 
+            System.Console.WriteLine();
 
+            // Creating a name generator from an XML definiton file
             var file = new XmlFileLoader("data/basic.xml");
             file.Load();
 
             var names2 = file.GetNameGenerator("SoftNameGenerator");
 
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 10; i++)
             {
                 System.Console.WriteLine(names2.Next());
             }
+
+            System.Console.WriteLine();
+
+            // Creating variations of a single name
+            var set = new HashSet<string>();
+            var syllableShifter = new SyllableShifter(provider);
+            var letterShifter = new LetterShifter(provider);
+            names.MinimumSyllables = 2;
+            names.MaximumSyllables = 2;
+            
+            for (int i = 0; i < 10; i++)
+            {
+                var sourceName = names.NextName();
+                set.Add(sourceName.ToString());
+
+                for (int j = 0; j < 5; j++)
+                {
+                    set.Add(letterShifter.NextVariation(sourceName).ToString());
+                }
+            }
+
+            foreach(var name in set.ToList().OrderBy(x => x))
+            {
+                System.Console.WriteLine(name);
+            }
+
         }
     }
 }
