1c6bfcc01648e91066c5b371f105e4c7e1dbcdbe
kesac <kevin@turtlesort.com>
2020-09-13 15:56:09 -0600

    StandaloneSyllableProvider is now DefaultSyllableProvider to indicate its use in default configurations of a NameGenerator

--- Syllabore/Syllabore/DefaultSyllableProvider.cs
+++ Syllabore/Syllabore/DefaultSyllableProvider.cs
@@ -8,7 +8,7 @@ namespace Syllabore
     /// A quick and dirty standlone syllable provider for quick use. Custom providers
     /// should use <c>ConfigurableSyllableProvider</c> instead.
     /// </summary>
-    public class StandaloneSyllableProvider : ConfigurableSyllableProvider
+    public class DefaultSyllableProvider : ConfigurableSyllableProvider
     {
 
         private static readonly string[] LeadingConsonants = { "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z" };
@@ -18,7 +18,7 @@ namespace Syllabore
         private static readonly string[] TrailingConsonants = { "B", "C", "D", "F", "G", "H", "K", "L", "M", "N", "P", "R", "S", "T", "V", "X", "Y"};
         private static readonly string[] TrailingConsonantSequences = { "CK", "ST", "SC", "NG", "NK", "RSH", "LSH", "RK", "RST", "NCT", "XT" };
 
-        public StandaloneSyllableProvider()
+        public DefaultSyllableProvider()
         {
 
             this.AddLeadingConsonant(LeadingConsonants);
