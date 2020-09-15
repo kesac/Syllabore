bdc8f75b838e04a25491d6f2a1359142953a3ba5
kesac <kevin@turtlesort.com>
2020-09-14 22:57:18 -0600

    Added string class for new extension method Atomize()

---
+++ Syllabore/Syllabore/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Syllabore
+{
+    public static class StringExtensions
+    {
+        public static string[] Atomize(this string s)
+        {
+            return s.Select(x => x.ToString()).ToArray();
+        }
+    }
+}
