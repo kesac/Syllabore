2fa4f851c9c45a0b98bb0e6eb52b59b3537676e2
kesac <kevin@turtlesort.com>
2020-09-17 22:51:25 -0600

    Added new string extensions to check whether a string ends with a vowel or consonant

--- Syllabore/Syllabore/StringExtensions.cs
+++ Syllabore/Syllabore/StringExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Syllabore
 {
@@ -11,5 +12,26 @@ namespace Syllabore
         {
             return s.Select(x => x.ToString()).ToArray();
         }
+
+        public static bool EndsWithVowel(this string s)
+        {
+            return Regex.IsMatch(s, "[aieouAEIOU]$");
+        }
+
+        public static bool EndsWithVowel(this Name n)
+        {
+            return Regex.IsMatch(n.ToString(), "[aieouAEIOU]$");
+        }
+
+        public static bool EndsWithConsonant(this string s)
+        {
+            return !s.EndsWithVowel();
+        }
+
+        public static bool EndsWithConsonant(this Name n)
+        {
+            return !n.EndsWithVowel();
+        }
+
     }
 }
