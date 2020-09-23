433b47c3dd4ce9bad5ec2cac95e9ec1a382c1b7d
kesac <kevin@turtlesort.com>
2020-09-22 18:37:49 -0600

    Added new constructor to Name class useful when running a desired name through mutators to produce variations

--- Syllabore/Syllabore/Name.cs
+++ Syllabore/Syllabore/Name.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Syllabore
 {
@@ -12,15 +10,37 @@ namespace Syllabore
     {
         public List<string> Syllables { get; set; }
 
+        /// <summary>
+        /// An empty name.
+        /// </summary>
         public Name()
         {
             this.Syllables = new List<string>();
         }
 
+        /// <summary>
+        /// A name with the desired starting syllables.
+        /// </summary>
+        /// <param name="syllable"></param>
+        public Name(params string[] syllable)
+        {
+            this.Syllables = new List<string>(syllable);
+        }
+
+        /// <summary>
+        /// A name that is a copy of the specified name. (This constructor
+        /// is useful for mutators.)
+        /// </summary>
+        /// <param name="copy"></param>
         public Name(Name copy) {
             this.Syllables = new List<string>(copy.Syllables);
         }
 
+        /// <summary>
+        /// Sequences the syllables of this Name into a single
+        /// string and then capitalizes it.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             var result = string.Join(string.Empty, this.Syllables);
