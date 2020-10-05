b66785e1b9654618494f8d16c79876643fab6722
kesac <kevin@turtlesort.com>
2020-10-05 14:25:56 -0600

    ConfigurationFile uses a standard set of JsonSerializerOptions now

--- Syllabore/Syllabore/ConfigurationFile.cs
+++ Syllabore/Syllabore/ConfigurationFile.cs
@@ -3,22 +3,26 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Syllabore
 {
+
     public static class ConfigurationFile
     {
+        private static readonly JsonSerializerOptions Options = new JsonSerializerOptions() { WriteIndented = true,  };
 
         public static void Save(NameGenerator generator, string path)
-        {
-            string result = JsonSerializer.Serialize<NameGenerator>(generator, new JsonSerializerOptions() { WriteIndented = true });
+        {    
+            string result = JsonSerializer.Serialize<NameGenerator>(generator, Options);
+            
             File.WriteAllText(path, result);
         }
 
         public static NameGenerator Load(string path)
         {
             string result = File.ReadAllText(path);
-            return JsonSerializer.Deserialize<NameGenerator>(result);
+            return JsonSerializer.Deserialize<NameGenerator>(result, Options);
         }
 
     }
