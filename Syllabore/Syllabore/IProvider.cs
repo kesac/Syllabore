2a5a712144dde84c7caff29088fa26d9bd75d53f
kesac <kevin@turtlesort.com>
2020-10-05 14:19:49 -0600

    Moved properties out of IProvider and IValidator

--- Syllabore/Syllabore/IProvider.cs
+++ Syllabore/Syllabore/IProvider.cs
@@ -9,22 +9,6 @@ namespace Syllabore
     /// </summary>
     public interface IProvider
     {
-        List<string> LeadingConsonants { get; set; }
-        List<string> LeadingConsonantSequences { get; set; }
-        List<string> Vowels { get; set; }
-        List<string> VowelSequences { get; set; }
-        List<string> TrailingConsonants { get; set; }
-        List<string> TrailingConsonantSequences { get; set; }
-
-        bool UseStartingSyllableLeadingVowels { get; set; }
-        bool UseLeadingConsonants { get; set; }
-        bool UseLeadingConsonantSequences { get; set; }
-        bool UseVowelSequences { get; set; }
-        bool UseTrailingConsonants { get; set; }
-        bool UseTrailingConsonantSequences { get; set; }
-
-        SyllableProviderProbability Probability { get; set; }
-
         /// <summary>
         /// Generates a random syllable suitable for starting name.
         /// </summary>
