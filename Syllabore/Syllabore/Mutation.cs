5c7c0affeb63da01a92908d6ebeb88f582b3fd21
kesac <kevin@turtlesort.com>
2020-11-03 19:55:05 -0700

    Changed implementation of mutations so they can be serialized

--- Syllabore/Syllabore/Mutation.cs
+++ Syllabore/Syllabore/Mutation.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Syllabore
 {
-
     /// <summary>
     /// Used by NameGenerator through ConfigurableNameMutator to capture
     /// mutations to produce variations on names. This class also has
@@ -13,11 +13,59 @@ namespace Syllabore
     /// </summary>
     public class Mutation
     {
-        public Action<Name> Mutate { get; set; }
+        public List<MutationStep> Steps { get; set; }
+
+        [JsonIgnore]
         public Func<Name, bool> CanMutate { get; set; }
-        public Mutation(Action<Name> mutate)
+
+        public Mutation()
         {
-            this.Mutate = mutate;
+            this.Steps = new List<MutationStep>();
         }
+        public void Apply(Name name)
+        {
+            foreach (var step in this.Steps)
+            {
+                step.Apply(name);
+            }
+        }
+
+        public Mutation When(Func<Name, bool> when)
+        {
+            this.CanMutate = when;
+            return this;
+        }
+
+        public Mutation ReplaceSyllable(int index, string replacement)
+        {
+            this.Steps.Add(new MutationStep(MutationStepType.SyllableReplacement, index.ToString(), replacement));
+            return this;
+        }
+
+        public Mutation InsertSyllable(int index, string syllable)
+        {
+            this.Steps.Add(new MutationStep(MutationStepType.SyllableInsertion, index.ToString(), syllable));
+            return this;
+        }
+
+        public Mutation AppendSyllable(string syllable)
+        {
+            this.Steps.Add(new MutationStep(MutationStepType.SyllableAppend, syllable));
+            return this;
+        }
+
+        public Mutation RemoveSyllable(int index)
+        {
+            this.Steps.Add(new MutationStep(MutationStepType.SyllableRemoval, index.ToString()));
+            return this;
+        }
+
+        public Mutation ExecuteUnserializableAction(Action<Name> unserializableAction)
+        {
+            this.Steps.Add(new MutationStep(unserializableAction));
+            return this;
+        }
+
+
     }
 }
---
+++ Syllabore/Syllabore/MutationStep.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syllabore
+{
+    public enum MutationStepType
+    {
+        SyllableInsertion,
+        SyllableAppend,
+        SyllableReplacement,
+        SyllableRemoval,
+        Lambda // Note: this cannot be serialized
+    }
+
+    public class MutationStep
+    {
+        public MutationStepType Type { get; set; }
+        public List<string> Arguments { get; set; }
+        private Action<Name> UnserializableAction { get; set; }
+
+        public MutationStep()
+        {
+            this.Arguments = new List<string>();
+        }
+
+        public MutationStep(MutationStepType type, params string[] args)
+        {
+            this.Type = type;
+            this.Arguments = new List<string>(args);
+        }
+
+        public MutationStep(Action<Name> unserializableAction)
+        {
+            this.Type = MutationStepType.Lambda;
+            this.UnserializableAction = unserializableAction;
+        }
+
+        public void Apply(Name name)
+        {
+            if(this.Type == MutationStepType.SyllableInsertion)
+            {
+                int index = int.Parse(this.Arguments[0]);
+                string insertion = this.Arguments[1];
+
+                if (index < 0)
+                {
+                    index = index + name.Syllables.Count;
+                }
+
+                name.Syllables.Insert(index, insertion);
+            }
+            else if (this.Type == MutationStepType.SyllableAppend)
+            {
+                string append = this.Arguments[0];
+
+                name.Syllables.Add(append);
+            }
+            else if (this.Type == MutationStepType.SyllableReplacement)
+            {
+                int index = int.Parse(this.Arguments[0]);
+                string replacement = this.Arguments[1];
+
+                if(index < 0)
+                {
+                    index = index + name.Syllables.Count;
+                }
+
+                name.Syllables[index] = replacement;
+            }
+            else if (this.Type == MutationStepType.SyllableRemoval)
+            {
+                int index = int.Parse(this.Arguments[0]);
+
+                if (index < 0)
+                {
+                    index = index + name.Syllables.Count;
+                }
+
+                name.Syllables.RemoveAt(index);
+            }
+            else if(this.Type == MutationStepType.Lambda)
+            {
+                UnserializableAction(name);
+            }
+
+        }
+
+    }
+}
