180942b364b7a76e12b95df833aca1ee03e5f0cf
kesac <kesac@users.noreply.github.com>
2021-10-30 13:26:12 -0600

    Added interface for weighted entities + Interface represents any entity (or choice) that can be randomly selected from a list and has a Weight property that is used to influence how frequently it should be chosen over others

---
+++ Syllabore/Syllabore/IWeighted.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syllabore
+{
+    /// <summary>
+    /// Represents a choice or entity that can
+    /// be randomly selected from a list, and has 
+    /// a weight value that affects how frequently it
+    /// is selected compared to others.
+    /// </summary>
+    public interface IWeighted
+    {
+        int Weight { get; set; }
+    }
+}
