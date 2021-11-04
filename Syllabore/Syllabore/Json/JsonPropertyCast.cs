31e524a0b4a87976c4e2fb98574031c6f84e06bb
kesac <kesac@users.noreply.github.com>
2021-11-03 20:13:51 -0600

    Serialization enhancements: + ConfigurationFile renamed to NameGeneratorConfig + Added JsonPropertyCast<T> as a way to solve polymorphic deserialization + NameGeneratorConfig now handles recent genericization of NameGenerator

---
+++ Syllabore/Syllabore/Json/JsonPropertyCast.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Syllabore.Json
+{
+    public class JsonPropertyCast<T> : JsonConverter<T>
+    {
+        private Type TargetType;
+
+        public JsonPropertyCast(Type targetType)
+        {
+            this.TargetType = targetType;
+        }
+        
+        public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return (T)JsonSerializer.Deserialize(reader.GetString(), this.TargetType, options);
+        }
+
+        public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(JsonSerializer.Serialize(value, this.TargetType, options));
+        }
+    }
+}
---
+++ Syllabore/Syllabore/Json/NameGeneratorConfig.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Syllabore.Json
+{
+
+    /// <summary>
+    /// A convenience class for reading and writing NameGenerator configurations to disk and back.
+    /// </summary>
+    public static class NameGeneratorConfig
+    {
+        /// <summary>
+        /// Writes the specified NameGenerator to disk as a JSON file to the specified file path.
+        /// The NameGenerator's provider, transformer, and filter are assumed be of type <see cref="SyllableProvider"/>,
+        /// <see cref="NameTransformer"/>, and <see cref="NameFilter"/> respectively.
+        /// </summary>
+        public static void Save(NameGenerator generator, string path)
+        {    
+            Save(generator, path, typeof(SyllableProvider), typeof(NameTransformer), typeof(NameFilter));
+        }
+
+        /// <summary>
+        /// Writes the specified NameGenerator to disk as a JSON file to the specified file path.
+        /// The NameGenerator's provider, transformer, and filter will be saved as the specified types.
+        /// </summary>
+        public static void Save(NameGenerator generator, string path, Type provider, Type transformer, Type filter)
+        {
+            var options = new JsonSerializerOptions() { WriteIndented = true };
+            options.Converters.Add(new JsonPropertyCast<ISyllableProvider>(provider));
+            options.Converters.Add(new JsonPropertyCast<INameTransformer>(transformer));
+            options.Converters.Add(new JsonPropertyCast<INameFilter>(filter));
+
+            string result = JsonSerializer.Serialize<NameGenerator>(generator, options);
+            File.WriteAllText(path, result);
+        }
+
+        /// <summary>
+        /// Reads a JSON file at the specified path and returns a <see cref="NameGenerator"/> based on that file.
+        /// The NameGenerator's provider, transformer, and filter will be instantiated as a <see cref="SyllableProvider"/>,
+        /// <see cref="NameTransformer"/>, and <see cref="NameFilter"/> respectively.
+        /// </summary>
+        public static NameGenerator Load(string path)
+        {
+            return Load(path, typeof(SyllableProvider), typeof(NameTransformer), typeof(NameFilter));
+        }
+
+        /// <summary>
+        /// Reads a JSON file at the specified path and returns a <see cref="NameGenerator"/> based on that file.
+        /// The NameGenerator's provider, transformer, and filter will be instantiated as the specified types.
+        /// </summary>
+        public static NameGenerator Load(string path, Type provider, Type transformer, Type filter)
+        {
+            var options = new JsonSerializerOptions() { WriteIndented = true };
+            options.Converters.Add(new JsonPropertyCast<ISyllableProvider>(provider));
+            options.Converters.Add(new JsonPropertyCast<INameTransformer>(transformer));
+            options.Converters.Add(new JsonPropertyCast<INameFilter>(filter));
+
+            string result = File.ReadAllText(path);
+            var g = JsonSerializer.Deserialize<NameGenerator>(result, options);
+
+            // The parent property is not serialized because it would create a cycle
+            // and so needs to be set again.
+            if(g.Provider is SyllableProvider)
+            {
+                var p = (SyllableProvider)g.Provider;
+                p.Probability.StartingSyllable.Parent = p.Probability;
+            }
+            
+            return g;
+        }
+
+    }
+}
