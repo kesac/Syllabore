2d972d02898b35f05e6d9f14192098ce8bd60b7e
kesac <kesac@users.noreply.github.com>
2021-11-03 20:17:50 -0600

    StringExtensions refactoring + A bunch of string extension methods in project Syllabore weren't being used anywhere except in unit tests so they've been moved to project Syllabore.Tests + Removed unit tests in StringExtensionTests because it wasn't providing any coverage for core classes of Syllabore

---
+++ Syllabore/Syllabore.Tests/StringExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Syllabore.Tests
+{
+    public static class StringExtensions
+    {
+
+        public static bool StartsWith(this string s, params string[] prefixes)
+        {
+            foreach (var prefix in prefixes)
+            {
+                if (s.StartsWith(prefix))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool ContainsAny(this string s, params string[] substrings)
+        {
+            foreach (var substring in substrings)
+            {
+                if (s.Contains(substring))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool EndsWith(this string s, params string[] suffixes)
+        {
+            foreach (var suffix in suffixes)
+            {
+                if (s.EndsWith(suffix))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
