b2f8a88db5cc8bea150649a5d6bdc65d549876d4
kesac <kesac@users.noreply.github.com>
2021-11-03 20:19:59 -0600

    Add property to INameTransformer + Moved property TransformChance from NameTransformer into its interface, INameTransformer which better supports the genericization of the NameGenerator class

--- Syllabore/Syllabore/INameTransformer.cs
+++ Syllabore/Syllabore/INameTransformer.cs
@@ -9,6 +9,16 @@ namespace Syllabore
     /// </summary>
     public interface INameTransformer
     {
+        /// <summary>
+        /// A number from 0 to 1 inclusive that represents the probablity
+        /// that a NameGenerator mutator will apply a transform from this
+        /// transformer during name generation (during a call to Next() or
+        /// NextName()).
+        /// A value of 0 means a mutation can never occur and a value of 1
+        /// means a mutation will always occur.
+        /// </summary>
+        double? TransformChance { get; set; }
+
         Name Transform(Name sourceName);
     }
 }
