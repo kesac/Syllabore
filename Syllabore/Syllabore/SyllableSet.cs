efa13f2c4eda4a73b4e0b339e66b7751de309fa5
kesac <kevin@turtlesort.com>
2023-03-31 23:24:39 -0600

    Improvements to SyllableSet + Added method for specifying starter, middle, or ending syllables + Specifying syllables or a SyllableProvider can be done in any order + Added docs

--- Syllabore/Syllabore/SyllableSet.cs
+++ Syllabore/Syllabore/SyllableSet.cs
@@ -5,70 +5,192 @@ using System.Text.Json.Serialization;
 
 namespace Syllabore
 {
-    // This type of provider constructs a finite set of syllables
-    // and only returns syllables from that set. This gives
-    // the appearance of cohesion across all output and is useful
-    // for simulating names that might come from a specific region.
+    /// <summary>
+    /// <para>
+    /// A special kind of syllable provider that constructs a finite
+    /// set of syllables and only returns syllables from that set.
+    /// A <see cref="SyllableSet"/> can be used as the syllable provider for
+    /// a <see cref="NameGenerator"/>.
+    /// </para>
+    /// <para>
+    /// Names constructed from a syllable set can give the appearance
+    /// of cohesion as if they originate from a similar geographic region,
+    /// culture, historical period, etc.
+    /// </para>
+    /// </summary>
     public class SyllableSet : ISyllableProvider
     {
-
         public int StartingSyllableMax { get; set; }
-        public int NormalSyllableMax { get; set; }
+        public int MiddleSyllableMax { get; set; }
         public int EndingSyllableMax { get; set; }
 
+        private ISyllableProvider Provider { get; set; }
+
         public HashSet<string> StartingSyllables { get; set; }
-        public HashSet<string> NormalSyllables { get; set; }
+        public HashSet<string> MiddleSyllables { get; set; }
         public HashSet<string> EndingSyllables { get; set; }
 
-        public SyllableSet() : this(8, 8, 8) { }
+        /// <summary>
+        /// Instantiates a new syllable set with a default size of
+        /// 8 starting syllables, 8 middle syllables, and 8 ending syllables.
+        /// The <see cref="DefaultSyllableProvider"/> is used to construct the
+        /// syllables.
+        /// </summary>
+        public SyllableSet() : this(8, 8, 8) 
+        {
+            // Purposely empty
+        }
 
-        public SyllableSet(int startingMax, int normalMax, int endingMax)
+        /// <summary>
+        /// Instantiates a new syllable set with the specified sizes.
+        /// A <see cref="DefaultSyllableProvider"/> is used to construct the
+        /// syllables unless replaced with a call to <see cref="WithProvider(ISyllableProvider)"/>.
+        /// </summary>
+        public SyllableSet(int startingSyllableCount, int middleSyllableCount, int endingSyllableCount)
         {
             this.StartingSyllables = new HashSet<string>();
-            this.NormalSyllables = new HashSet<string>();
+            this.MiddleSyllables = new HashSet<string>();
             this.EndingSyllables = new HashSet<string>();
 
-            this.StartingSyllableMax = startingMax;
-            this.NormalSyllableMax = normalMax;
-            this.EndingSyllableMax = endingMax;
+            this.StartingSyllableMax = startingSyllableCount;
+            this.MiddleSyllableMax = middleSyllableCount;
+            this.EndingSyllableMax = endingSyllableCount;
+
+            this.WithProvider(new DefaultSyllableProvider());
         }
 
-        public SyllableSet InitializeWith(Func<SyllableProvider, SyllableProvider> config)
+        /// <summary>
+        /// Uses the specified <see cref="SyllableProvider"/> to create this
+        /// syllable set's finite pool of syllables.
+        /// </summary>
+        public SyllableSet WithProvider(Func<SyllableProvider, SyllableProvider> config)
         {
-            this.InitializeWith(config(new SyllableProvider()));
+            this.WithProvider(config(new SyllableProvider()));
             return this;
         }
 
-        public SyllableSet InitializeWith(ISyllableProvider provider)
+        /// <summary>
+        /// Uses the specified <see cref="ISyllableProvider"/> to create this
+        /// syllable set's finite pool of syllables.
+        /// </summary>
+        public SyllableSet WithProvider(ISyllableProvider provider)
         {
+            this.Provider = provider;
+            return this;
+        }
 
-            this.StartingSyllables.Clear();
-            for (int i = 0; i < this.StartingSyllableMax; i++)
+        /// <summary>
+        /// Adds a syllable to this <see cref="SyllableSet"/>'s pool of starting syllables.
+        /// </summary>
+        /// <param name="syllables">One or more starting syllables to add this syllable set</param>
+        /// <exception cref="InvalidOperationException">
+        ///     Thrown if adding the specified starting syllables exceeds the maximum size of this
+        ///     syllable set.
+        /// </exception>
+        public SyllableSet WithStartingSyllable(params string[] syllables)
+        {
+            foreach (var syllable in syllables)
             {
-                this.StartingSyllables.Add(provider.NextStartingSyllable());
-            }
+                this.StartingSyllables.Add(syllable);
 
-            this.NormalSyllables.Clear();
-            for (int i = 0; i < this.NormalSyllableMax; i++)
-            {
-                this.NormalSyllables.Add(provider.NextSyllable());
-            }
+                if (this.StartingSyllables.Count > this.StartingSyllableMax)
+                {
+                    throw new InvalidOperationException("Exceeded limit for number of starting syllables");
+                }
 
-            this.EndingSyllables.Clear();
-            for (int i = 0; i < this.EndingSyllableMax; i++)
-            {
-                this.EndingSyllables.Add(provider.NextEndingSyllable());
             }
 
             return this;
         }
 
+        /// <summary>
+        /// Adds a syllable to this <see cref="SyllableSet"/>'s pool of "middle" (neither
+        /// starting nor ending) syllables.
+        /// </summary>
+        /// <param name="syllables">One or more middle syllables to add this syllable set</param>
+        /// <exception cref="InvalidOperationException">
+        ///     Thrown if adding the specified middle syllables exceeds the maximum size of this
+        ///     syllable set.
+        /// </exception>
+        public SyllableSet WithMiddleSyllable(params string[] syllables)
+        {
+            foreach (var syllable in syllables)
+            {
+                this.MiddleSyllables.Add(syllable);
 
-        public string NextStartingSyllable() => this.StartingSyllables.RandomItem<string>();
+                if (this.MiddleSyllables.Count > this.MiddleSyllableMax)
+                {
+                    throw new InvalidOperationException("Exceeded limit for number of middle syllables");
+                }
+            }
 
-        public string NextSyllable() => this.NormalSyllables.RandomItem<string>();
+            return this;
+        }
 
-        public string NextEndingSyllable() => this.EndingSyllables.RandomItem<string>();
+        /// <summary>
+        /// Adds a syllable to this <see cref="SyllableSet"/>'s pool of ending syllables.
+        /// </summary>
+        /// <param name="syllables">One or more ending syllables to add this syllable set</param>
+        /// <exception cref="InvalidOperationException">
+        ///     Thrown if adding the specified ending syllables exceeds the maximum size of this
+        ///     syllable set.
+        /// </exception>
+        public SyllableSet WithEndingSyllable(params string[] syllables)
+        {
+            foreach (var syllable in syllables)
+            {
+                this.EndingSyllables.Add(syllable);
+
+                if (this.EndingSyllables.Count > this.EndingSyllableMax)
+                {
+                    throw new InvalidOperationException("Exceeded limit for number of ending syllables");
+                }
+            }
+
+            return this;
+        }
+
+        // Inherited
+        public string NextStartingSyllable()
+        {
+            if (this.StartingSyllables.Count < this.StartingSyllableMax)
+            {
+                for (int i = this.StartingSyllables.Count; i < this.StartingSyllableMax; i++)
+                {
+                    this.StartingSyllables.Add(this.Provider.NextStartingSyllable());
+                }
+            }
+
+            return this.StartingSyllables.RandomItem<string>();
+        }
+
+        // Inherited
+        public string NextSyllable()
+        {
+            if (this.MiddleSyllables.Count < this.MiddleSyllableMax)
+            {
+                for (int i = this.MiddleSyllables.Count; i < this.MiddleSyllableMax; i++)
+                {
+                    this.MiddleSyllables.Add(this.Provider.NextSyllable());
+                }
+            }
+
+            return this.MiddleSyllables.RandomItem<string>();
+        }
+
+        // Inherited
+        public string NextEndingSyllable()
+        {
+            if (this.EndingSyllables.Count < this.EndingSyllableMax)
+            {
+                for (int i = this.EndingSyllables.Count; i < this.EndingSyllableMax; i++)
+                {
+                    this.EndingSyllables.Add(this.Provider.NextEndingSyllable());
+                }
+            }
+
+            return this.EndingSyllables.RandomItem<string>();
+        }
 
     }
 }
