c0ba0484e4ac48164230e9f8d872ebf315fe5e15
kesac <kevin@turtlesort.com>
2023-04-02 15:01:51 -0600

    Added name formatters + These are a convenient way to combine one or more name generators

---
+++ Syllabore/Syllabore/NameFormatter.cs
@@ -0,0 +1,84 @@
+﻿using Archigen;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Syllabore
+{
+    /// <summary>
+    /// A convenience class used for modeling names that have multiple parts
+    /// and need multiple generators to create them.
+    /// </summary>
+    public class NameFormatter : IGenerator
+    {
+        private Random Random { get; set; }
+        public string Format { get; set; }
+        public Dictionary<string, INameGenerator> BoundNameGenerators { get; set; }
+
+        /// <summary>
+        /// <para>
+        /// Instantiates a new <see cref="NameFormatter"/> with the specified format. Substrings 
+        /// that need to be replaced with a generated name should be surrounded with curly brackets.
+        /// </para>
+        /// <para>
+        /// For example, the format <i>"John {middle-name} Smith"</i> tells a <see cref="NameFormatter"/> that the
+        /// name between the first and last needs to be generated.
+        /// </para>
+        /// </summary>
+        /// <param name="format"></param>
+        public NameFormatter(string format)
+        {
+            this.Format = format ?? throw new ArgumentNullException("format", "The desired format cannot be null");
+            this.BoundNameGenerators = new Dictionary<string, INameGenerator>();
+            this.Random = new Random();
+        }
+
+        /// <summary>
+        /// Specifies a <see cref="INameGenerator"/> for the specified property.
+        /// </summary>
+        public NameFormatter UsingGenerator(string property, INameGenerator generator)
+        {
+            this.BoundNameGenerators[property] = generator;
+            return this;
+        }
+
+        /// <summary>
+        /// Returns a new generated name based on the previously specified format.
+        /// </summary>
+        public string Next()
+        {
+            var result = new StringBuilder(this.Format);
+            var properties = this.GetProperties(this.Format);
+
+            foreach (var property in properties)
+            {
+                if (this.BoundNameGenerators.ContainsKey(property))
+                {
+                    result.Replace("{" + property + "}", this.BoundNameGenerators[property].Next());
+                }
+            }
+
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// Grabs substrings that were surrounded by curly brackets
+        /// and returns them in an array.
+        /// </summary>
+        private string[] GetProperties(string format)
+        {
+            var matches = Regex.Matches(format, "\\{.+?\\}");
+            var result = new string[matches.Count];
+            
+            for (int i = 0; i < matches.Count; i++)
+            {
+                // Remove the leading and trailing curly bracket
+                result[i] = matches[i].Value.Substring(1, matches[i].Length - 1 - 1);
+            }
+
+            return result;
+        }
+
+    }
+}
