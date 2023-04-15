7eaca05d2c0af2c17f837be2fdf6ecb8a4ffa685
kesac <kevin@turtlesort.com>
2023-04-15 01:36:23 -0600

    Deprecated ISyllableProvider: + Use ISyllableGenerator instead

--- Syllabore/Syllabore/ISyllableProvider.cs
+++ Syllabore/Syllabore/ISyllableProvider.cs
@@ -4,25 +4,6 @@ using System.Text;
 
 namespace Syllabore
 {
-    /// <summary>
-    /// Generates syllables that can be sequenced into names.
-    /// </summary>
-    public interface ISyllableProvider
-    {
-        /// <summary>
-        /// Generates a random syllable suitable for starting name.
-        /// </summary>
-        string NextStartingSyllable();
-
-        /// <summary>
-        /// Generates a random syllable suitable for any part of a name.
-        /// </summary>
-        string NextSyllable();
-
-        /// <summary>
-        /// Generates a random syllable suitable for ending a name.
-        /// </summary>
-        string NextEndingSyllable();
-
-    }
+    [Obsolete("Use ISyllableGenerator instead.", false)]
+    public interface ISyllableProvider : ISyllableGenerator { }
 }
