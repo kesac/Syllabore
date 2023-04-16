dd3f9465cfc204a8f54a0c2b85a4df643fc556c0
kesac <kevin@turtlesort.com>
2023-04-16 17:51:55 -0600

    Added docs to deprecated class and interface

--- Syllabore/Syllabore/ISyllableProvider.cs
+++ Syllabore/Syllabore/ISyllableProvider.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Syllabore
 {
+    /// <summary>
+    /// Deprecated. Use <see cref="ISyllableGenerator"/> instead.
+    /// </summary>
     [Obsolete("Use ISyllableGenerator instead.", false)]
     public interface ISyllableProvider : ISyllableGenerator { }
 }
--- Syllabore/Syllabore/SyllableProvider.cs
+++ Syllabore/Syllabore/SyllableProvider.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Syllabore
 {
+    /// <summary>
+    /// Deprecated. Use <see cref="SyllableGenerator"/> instead.
+    /// </summary>
     [Obsolete("Replaced by SyllableGenerator.", false)]
     public class SyllableProvider : SyllableGenerator { }
 }
