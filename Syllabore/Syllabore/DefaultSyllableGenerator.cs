42e67a963b0d30e8fa03dfe508985ac3bba6fac0
kesac <kevin@turtlesort.com>
2023-10-09 17:13:32 -0600

    Simplified the default syllable generator.

--- Syllabore/Syllabore/DefaultSyllableGenerator.cs
+++ Syllabore/Syllabore/DefaultSyllableGenerator.cs
@@ -9,30 +9,20 @@ namespace Syllabore
     /// The default syllable provider used by a vanilla instance
     /// of <see cref="NameGenerator"/>.
     /// </para>
-    /// <para>
-    /// All graphemes of the English language are used in this syllable provider.
-    /// </para>
     /// </summary>
     public class DefaultSyllableGenerator : SyllableGenerator
     {
-
-        private static readonly string[] DefaultLeadingConsonants = { "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z" };
-        private static readonly string[] DefaultLeadingConsonantSequences = { "CH", "SH", "BL", "CL", "FL", "PL", "GL", "BR", "CR", "DR", "PR", "TR", "TH", "SC", "SP", "ST", "SL", "SPR" };
-        private static readonly string[] DefaultVowels = { "A", "E", "I", "O", "U" };
-        private static readonly string[] DefaultVowelSequences = { "AE", "EA", "AI", "IA", "AU", "AY", "IE", "OI", "OU", "EY" };
-        private static readonly string[] DefaultTrailingConsonants = { "B", "C", "D", "F", "G", "H", "K", "L", "M", "N", "P", "R", "S", "T", "V", "X", "Y"};
-        private static readonly string[] DefaultTrailingConsonantSequences = { "CK", "ST", "SC", "NG", "NK", "RSH", "LSH", "RK", "RST", "NCT", "XT" };
-
         public DefaultSyllableGenerator()
         {
-            this.WithLeadingConsonants(DefaultLeadingConsonants)
-                .WithLeadingConsonantSequences(DefaultLeadingConsonantSequences)
-                .WithVowels(DefaultVowels)
-                .WithVowelSequences(DefaultVowelSequences)
-                .WithTrailingConsonants(DefaultTrailingConsonants)
-                .WithTrailingConsonantSequences(DefaultTrailingConsonantSequences);
+            this.WithLeadingConsonants("bdlmprst");
+            this.WithLeadingConsonantSequences("st", "ph", "br");
+            this.WithVowels("aeio");
+            this.WithFinalConsonants("dlmnrstx");
+            this.WithFinalConsonantSequences("st", "rn", "lt");
+            this.WithProbability(x => x
+                    .OfLeadingConsonantIsSequence(0.10)
+                    .OfFinalConsonants(0.50)
+                    .OfFinalConsonantIsSequence(0.25));
         }
-
-
     }
 }
