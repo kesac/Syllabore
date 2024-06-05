cfc160dfa833e6ee1d6c5610a97f2aad3826d0e8
kesac <kevin@turtlesort.com>
2024-06-04 19:12:06 -0600

    Added IRandomizable to make Random instances more accessible

---
+++ Syllabore/Syllabore/IRandomizable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Syllabore
+{
+    /// <summary>
+    /// Represents an entity that simulates
+    /// randomness using system class <see cref="System.Random"/>.
+    /// <para>
+    /// The purpose of this interface is to make the entity's 
+    /// instance of <see cref="System.Random"/>
+    /// available for retrieval and modification. This is important
+    /// for controlling seeds during testing.
+    /// </para>
+    /// </summary>
+    public interface IRandomizable
+    {
+        /// <summary>
+        /// The instance of <see cref="System.Random"/> used to 
+        /// simulate randomness.
+        /// </summary>
+        Random Random { get; set; }
+    }
+}
