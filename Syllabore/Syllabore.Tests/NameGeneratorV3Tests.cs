383d0badad2bad9616a1579654829b23533a18ae
kesac <kevin@turtlesort.com>
2025-03-06 22:13:18 -0700

    Added v3 NameGenerator along with a few unit tests

---
+++ Syllabore/Syllabore.Tests/NameGeneratorV3Tests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Syllabore.Tests
+{
+    [TestClass]
+    public class NameGeneratorV3Tests
+    {
+        [TestMethod]
+        public void Constructor_NoSyllableGenerators_ThrowsExceptionWhenGenerating()
+        {
+            var sut = new NameGeneratorV3();
+            Assert.ThrowsException<InvalidOperationException>(() => sut.Next());
+        }
+
+        [TestMethod]
+        public void Configuration_AddGeneratorForFirstPosition_GeneratesName()
+        {
+            var symbols = new SymbolGenerator().Add("a");
+            var syllables = new SyllableGeneratorV3().Add(Position.First, symbols);
+            var sut = new NameGeneratorV3()
+                .Add(Position.First, syllables)
+                .Size(1);
+
+            var name = sut.Next();
+            Assert.IsTrue(name.Equals("A"));
+        }
+
+        [TestMethod]
+        public void Configuration_AddGeneratorsForFirstAndLastPositions_GeneratesName()
+        {
+            var firstSymbols = new SymbolGenerator().Add("a");
+            var lastSymbols = new SymbolGenerator().Add("b");
+            var sut = new NameGeneratorV3()
+                .Add(Position.First, new SyllableGeneratorV3().Add(Position.First, firstSymbols))
+                .Add(Position.Last, new SyllableGeneratorV3().Add(Position.Last, lastSymbols));
+
+            var name = sut.Next();
+            Assert.IsTrue(name.Equals("Ab"));
+        }
+
+        [TestMethod]
+        public void Configuration_AddGeneratorsForAllPositions_GeneratesName()
+        {
+            var firstSymbols = new SymbolGenerator().Add("a");
+            var middleSymbols = new SymbolGenerator().Add("b");
+            var lastSymbols = new SymbolGenerator().Add("c");
+            var sut = new NameGeneratorV3()
+                .Add(Position.First, new SyllableGeneratorV3().Add(Position.First, firstSymbols))
+                .Add(Position.Middle, new SyllableGeneratorV3().Add(Position.Middle, middleSymbols))
+                .Add(Position.Last, new SyllableGeneratorV3().Add(Position.Last, lastSymbols))
+                .Size(3);
+
+            var name = sut.Next();
+            Assert.IsTrue(name.Equals("Abc"));
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2)]
+        [DataRow(2, 4)]
+        [DataRow(3, 10)]
+        public void Configuration_SetMinimumAndMaximumSize_GeneratesNameWithinSizeRange(int min, int max)
+        {
+            var firstSymbols = new SymbolGenerator().Add("a");
+            var middleSymbols = new SymbolGenerator().Add("b");
+            var lastSymbols = new SymbolGenerator().Add("c");
+
+            var sut = new NameGeneratorV3()
+                .Add(Position.First, new SyllableGeneratorV3().Add(Position.First, firstSymbols))
+                .Add(Position.Middle, new SyllableGeneratorV3().Add(Position.Middle, middleSymbols))
+                .Add(Position.Last, new SyllableGeneratorV3().Add(Position.Last, lastSymbols))
+                .Size(min, max);
+
+            // Test name generation multiple times to ensure correctness
+            for (int i = 0; i < 100; i++)
+            {
+                var name = sut.Next();
+                Assert.IsTrue(name.Length >= min && name.Length <= max);
+            }
+        }
+    }
+}
---
+++ Syllabore/Syllabore/NameGeneratorV3.cs
@@ -0,0 +1,141 @@
+﻿using Archigen;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syllabore
+{
+    /// <summary>
+    /// Generates names by constructing syllables and joining them together.
+    /// </summary>
+    public class NameGeneratorV3 : IGenerator<string>, IRandomizable
+    {
+        /// <summary>
+        /// The instance of <see cref="System.Random"/> used to simulate randomness.
+        /// </summary>
+        public Random Random { get; set; }
+
+        /// <summary>
+        /// The <see cref="SyllableGeneratorV3">SyllableGenerators</see> used by this <see cref="NameGeneratorV3"/>.
+        /// </summary>
+        public Dictionary<Position, SyllableGeneratorV3> SyllableGenerators { get; set; }
+
+        /// <summary>
+        /// The minimum number of syllables in generated names.
+        /// </summary>
+        public int MinimumSize { get; set; }
+
+        /// <summary>
+        /// The maximum number of syllables in generated names.
+        /// </summary>
+        public int MaximumSize { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NameGeneratorV3"/> class.
+        /// </summary>
+        public NameGeneratorV3()
+        {
+            SyllableGenerators = new Dictionary<Position, SyllableGeneratorV3>();
+            Random = new Random();
+            MinimumSize = 2;
+            MaximumSize = 3;
+        }
+
+        /// <summary>
+        /// Adds a <see cref="SyllableGeneratorV3"/> for the specified position.
+        /// </summary>
+        /// <param name="position">The position for the syllable generator.</param>
+        /// <param name="generator">The syllable generator to add.</param>
+        /// <returns>The current instance of <see cref="NameGeneratorV3"/>.</returns>
+        public NameGeneratorV3 Add(Position position, SyllableGeneratorV3 generator)
+        {
+            SyllableGenerators[position] = generator;
+            return this;
+        }
+
+        /// <summary>
+        /// Sets both the minimum and maximum number of syllables to use per name.
+        /// </summary>
+        public NameGeneratorV3 Size(int size)
+        {
+            MinimumSize = size;
+            MaximumSize = size;
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the minimum and maximum number of syllables to use per name.
+        /// </summary>
+        public NameGeneratorV3 Size(int minSize, int maxSize)
+        {
+            MinimumSize = minSize;
+            MaximumSize = maxSize;
+            return this;
+        }
+
+        /// <summary>
+        /// Generates a name by concatenating syllables from different positions based on the specified size.
+        /// </summary>
+        /// <returns>The generated name as a capitalized string.</returns>
+        public string Next()
+        {
+            if (SyllableGenerators.Count == 0)
+            {
+                throw new InvalidOperationException("No syllable generators available to generate a name.");
+            }
+
+            if (MinimumSize <= 0 || MaximumSize <= 0 || MinimumSize > MaximumSize)
+            {
+                throw new InvalidOperationException("Invalid size range.");
+            }
+
+            int size = Random.Next(MinimumSize, MaximumSize + 1);
+            var name = new StringBuilder();
+
+            if (size == 1)
+            {
+                if (SyllableGenerators.ContainsKey(Position.First))
+                {
+                    name.Append(SyllableGenerators[Position.First].Next());
+                }
+                else
+                {
+                    throw new InvalidOperationException("No syllable generator available for the first position.");
+                }
+            }
+            else if (size == 2)
+            {
+                if (SyllableGenerators.ContainsKey(Position.First) && SyllableGenerators.ContainsKey(Position.Last))
+                {
+                    name.Append(SyllableGenerators[Position.First].Next());
+                    name.Append(SyllableGenerators[Position.Last].Next());
+                }
+                else
+                {
+                    throw new InvalidOperationException("No syllable generator available for the first or last position.");
+                }
+            }
+            else
+            {
+                if (SyllableGenerators.ContainsKey(Position.First) && SyllableGenerators.ContainsKey(Position.Last) && SyllableGenerators.ContainsKey(Position.Middle))
+                {
+                    name.Append(SyllableGenerators[Position.First].Next());
+
+                    for (int i = 1; i < size - 1; i++)
+                    {
+                        name.Append(SyllableGenerators[Position.Middle].Next());
+                    }
+
+                    name.Append(SyllableGenerators[Position.Last].Next());
+                }
+                else
+                {
+                    throw new InvalidOperationException("Not enough SyllableGenerators to create a name of the desired size.");
+                }
+            }
+
+            // Capitalize the first character of the generated name
+            return name[0].ToString().ToUpper() + name.ToString(1, name.Length - 1);
+        }
+    }
+}
