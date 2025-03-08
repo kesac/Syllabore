aada5747d88dbcad9c2b3eb2e02fcd8231ad4448
kesac <kevin@turtlesort.com>
2025-03-07 22:50:06 -0700

    Split Position enum into SymbolPosition and SyllablePosition

---
+++ Syllabore/Syllabore/SyllablePosition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syllabore
+{
+    /// <summary>
+    /// <see cref="SyllablePositionV2"/>
+    /// </summary>
+    public enum SyllablePosition
+    {
+        Unknown,
+        Leading,
+        Inner,
+        Trailing,
+        Any
+    }
+}
---
+++ Syllabore/Syllabore/SymbolPosition.cs
@@ -0,0 +1,30 @@
+﻿namespace Syllabore
+{
+    /// <summary>
+    /// Represents the position of a symbol within a syllable.
+    /// </summary>
+    public enum SymbolPosition 
+    {
+        Unknown,
+
+        /// <summary>
+        /// The first symbol of a syllable.
+        /// </summary>
+        First,
+
+        /// <summary>
+        /// The position between the first and last symbols of a syllable.
+        /// </summary>
+        Middle,
+
+        /// <summary>
+        /// The last symbol of a syllable.
+        /// </summary>
+        Last,
+
+        /// <summary>
+        /// Represents any symbol position within a syllable or any syllable position within a name.
+        /// </summary>
+        Any
+    }
+}
