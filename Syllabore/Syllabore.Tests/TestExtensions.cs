1da42111a32ee5625b21318042b0d45d376efced
kesac <kevin@turtlesort.com>
2025-03-11 23:46:06 -0600

    Combined extension helper classes in unit test project

--- Syllabore/Syllabore.Tests/TestExtensions.cs
+++ Syllabore/Syllabore.Tests/TestExtensions.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
-namespace Syllabore.Tests
+namespace Syllabore
 {
-    public static class StringExtensions
+    public static class TestExtensions
     {
+
+        public static bool UnorderedListEquals(this List<FilterConstraint> first, List<FilterConstraint> second)
+        {
+            //return first.ToHashSet<Condition>().SetEquals(second.ToHashSet<Condition>());
+            return first.Select(x => (x.Type, x.Value)).ToHashSet().SetEquals(second.Select(x => (x.Type, x.Value)).ToHashSet());
+        }
+
+        public static bool UnorderedListEquals(this List<Symbol> first, List<Symbol> second)
+        {
+            return first.Select(x => x.Value).ToHashSet<string>().SetEquals(second.Select(x => x.Value).ToHashSet<string>());
+        }
 
         public static bool StartsWith(this string s, params string[] prefixes)
         {
