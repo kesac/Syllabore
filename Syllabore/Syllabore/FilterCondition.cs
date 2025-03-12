2aff98a9d534c7c36dbe61f13335213a4f111a38
kesac <kevin@turtlesort.com>
2025-03-11 23:05:23 -0600

    Moved filter conditions and constraints to their own file

---
+++ Syllabore/Syllabore/FilterCondition.cs
@@ -0,0 +1,29 @@
+﻿namespace Syllabore
+{
+    /// <summary>
+    /// The type of condition that a 
+    /// <see cref="FilterConstraint"/> uses.
+    /// </summary>
+    public enum FilterCondition
+    {
+        /// <summary>
+        /// Condition is met if the name contains a specific substring.
+        /// </summary>
+        Contains,
+
+        /// <summary>
+        /// Condition is met if the name starts with a specific substring.
+        /// </summary>
+        StartsWith,
+
+        /// <summary>
+        /// Condition is met if the name ends with a specific substring.
+        /// </summary>
+        EndsWith,
+
+        /// <summary>
+        /// Condition is met if the name matches a specific regular expression.
+        /// </summary>
+        MatchesPattern
+    }
+}
---
+++ Syllabore/Syllabore/FilterConstraint.cs
@@ -0,0 +1,31 @@
+﻿namespace Syllabore
+{
+    /// <summary>
+    /// A constraint used by a <see cref="NameFilter"/> when
+    /// testing names for validity.
+    /// </summary>
+    public class FilterConstraint
+    {
+        /// <summary>
+        /// The type of condition names will be tested against.
+        /// (eg. Contains, StartsWith, EndsWith, MatchesPattern)
+        /// </summary>
+        public FilterCondition Type { get; set; }
+
+        /// <summary>
+        /// The value that names will be tested against (in conjunction
+        /// with <see cref="Type"/>).
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Creates a new <see cref="FilterConstraint"/> with the specified condition and value.
+        /// </summary>
+        public FilterConstraint(FilterCondition type, string value)
+        {
+            this.Type = type;
+            this.Value = value;
+        }
+
+    }
+}
