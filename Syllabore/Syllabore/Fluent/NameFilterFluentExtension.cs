08ea539117d41361e8dc4ae0b652c6ed7a1263ad
kesac <kevin@turtlesort.com>
2025-03-11 23:06:17 -0600

    Simplified NameFilter and moved fluent convenience methods to optional class

---
+++ Syllabore/Syllabore/Fluent/NameFilterFluentExtension.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syllabore.Fluent
+{
+    public static class NameFilterFluentExtension
+    {
+        /// <summary>
+        /// Makes a name invalid if it contains any of the specified substrings.
+        /// </summary>
+        public static NameFilter DoNotAllowSubstring(this NameFilter filter, params string[] substring)
+        {
+            foreach (string s in substring)
+            {
+                filter.Add(new FilterConstraint(FilterCondition.Contains, s));
+            }
+
+            return filter;
+        }
+
+        /// <summary>
+        /// Makes a name invalid if it matches any of the specified regular expressions.
+        /// </summary>
+        public static NameFilter DoNotAllowRegex(this NameFilter filter, params string[] regex)
+        {
+            foreach (string r in regex)
+            {
+                filter.Add(new FilterConstraint(FilterCondition.MatchesPattern, r));
+            }
+
+            return filter;
+        }
+
+        /// <summary>
+        /// Makes a name invalid if it starts with any of the specified substrings.
+        /// </summary>
+        public static NameFilter DoNotAllowStart(this NameFilter filter, params string[] prefixes)
+        {
+            foreach (string s in prefixes)
+            {
+                filter.Add(new FilterConstraint(FilterCondition.StartsWith, s));
+            }
+
+            return filter;
+        }
+
+        /// <summary>
+        /// Makes a name invalid if it ends with any of the specified substrings.
+        /// </summary>
+        public static NameFilter DoNotAllowEnding(this NameFilter filter, params string[] suffixes)
+        {
+            foreach (string s in suffixes)
+            {
+                filter.Add(new FilterConstraint(FilterCondition.EndsWith, s));
+            }
+
+            return filter;
+        }
+    }
+}
--- Syllabore/Syllabore/NameFilter.cs
+++ Syllabore/Syllabore/NameFilter.cs
@@ -5,61 +5,6 @@ using System.Text.RegularExpressions;
 
 namespace Syllabore
 {
-    /// <summary>
-    /// The type of condition that a 
-    /// <see cref="FilterConstraint"/> uses.
-    /// </summary>
-    public enum FilterCondition
-    {
-        /// <summary>
-        /// Condition is met if the name contains a specific substring.
-        /// </summary>
-        Contains,
-
-        /// <summary>
-        /// Condition is met if the name starts with a specific substring.
-        /// </summary>
-        StartsWith,
-
-        /// <summary>
-        /// Condition is met if the name ends with a specific substring.
-        /// </summary>
-        EndsWith,
-
-        /// <summary>
-        /// Condition is met if the name matches a specific regular expression.
-        /// </summary>
-        MatchesPattern
-    }
-
-    /// <summary>
-    /// A constraint used by a <see cref="NameFilter"/> when
-    /// testing names for validity.
-    /// </summary>
-    public class FilterConstraint
-    {
-        /// <summary>
-        /// The type of condition names will be tested against.
-        /// (eg. Contains, StartsWith, EndsWith, MatchesPattern)
-        /// </summary>
-        public FilterCondition Type { get; set; }
-
-        /// <summary>
-        /// The value that names will be tested against (in conjunction
-        /// with <see cref="Type"/>).
-        /// </summary>
-        public string Value { get; set; }
-
-        /// <summary>
-        /// Creates a new <see cref="FilterConstraint"/> with the specified condition and value.
-        /// </summary>
-        public FilterConstraint(FilterCondition type, string value)
-        {
-            this.Type = type;
-            this.Value = value;
-        }
-
-    }
 
     /// <summary>
     /// Validates names produced by a <see cref="INameGenerator"/> against a set of configurable constraints.
@@ -79,80 +24,42 @@ namespace Syllabore
         {
             this.Constraints = new List<FilterConstraint>();
         }
-
-        /// <summary>
-        /// Makes a name invalid if it contains any of the specified substrings.
-        /// </summary>
-        public NameFilter DoNotAllowSubstring(params string[] substring)
+        public NameFilter Add(params string[] patterns)
         {
-            foreach(string s in substring)
+            foreach (var value in patterns)
             {
-                this.Constraints.Add(new FilterConstraint(FilterCondition.Contains, s));
+                this.Add(new FilterConstraint(FilterCondition.MatchesPattern, value));
+
             }
 
             return this;
         }
 
-        /// <summary>
-        /// Makes a name invalid if it matches any of the specified regular expressions.
-        /// </summary>
-        public NameFilter DoNotAllow(params string[] regex)
+        public NameFilter Add(FilterConstraint constraint)
         {
-            foreach(string r in regex)
-            {
-                this.Constraints.Add(new FilterConstraint(FilterCondition.MatchesPattern, r));
-            }
-
+            this.Constraints.Add(constraint);
             return this;
         }
 
-        /// <summary>
-        /// Deprecated. Replaced with DoNotAllow().
-        /// </summary>
-        [Obsolete("Replaced with DoNotAllow()", false)]
-        public NameFilter DoNotAllowPattern(params string[] regex)
-        {
-            return this.DoNotAllow(regex);
-        }
-
-        /// <summary>
-        /// Makes a name invalid if it starts with any of the specified substrings.
-        /// </summary>
-        public NameFilter DoNotAllowStart(params string[] prefixes)
-        {
-            foreach (string s in prefixes)
-            {
-                this.Constraints.Add(new FilterConstraint(FilterCondition.StartsWith, s));
-            }
-
-            return this;
-        }
-
-        /// <summary>
-        /// Makes a name invalid if it ends with any of the specified substrings.
-        /// </summary>
-        public NameFilter DoNotAllowEnding(params string[] suffixes)
-        {
-            foreach(string s in suffixes)
-            {
-                this.Constraints.Add(new FilterConstraint(FilterCondition.EndsWith, s));
-                //this.Conditions.Add(s.Trim() + "$");
-            }
-
-            return this;
-        }
-
+        // TODO: Do we still need Name objects?
         /// <summary>
         /// Returns true if the specified name does not match any of this filter's contraints, else returns false.
         /// </summary>
         public bool IsValidName(Name name)
         {
+            return IsValid(name.ToString());
+        }
 
+        /// <summary>
+        /// Returns true if the specified name does not match any of this filter's contraints, else returns false.
+        /// </summary>
+        public bool IsValid(string name)
+        {
             bool isValid = true;
 
             foreach (var c in this.Constraints)
             {
-                var lowercaseName = name.ToString().ToLower();
+                var lowercaseName = name.ToLower();
                 var lowercaseValue = c.Value.ToLower();
 
                 if (c.Type == FilterCondition.Contains && lowercaseName.Contains(lowercaseValue))
@@ -172,14 +79,16 @@ namespace Syllabore
                     isValid = false;
                 }
 
-                if(!isValid)
+                if (!isValid)
                 {
                     break;
                 }
 
             }
+
             return isValid;
         }
+
 
     }
 }
