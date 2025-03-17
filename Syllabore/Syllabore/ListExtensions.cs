344fb26d8c6199c99ba3804cbf2c9c93afc35d82
kesac <kevin@turtlesort.com>
2025-03-16 18:51:17 -0600

    Removed unused extension methods for lists

--- Syllabore/Syllabore/ListExtensions.cs
+++ Syllabore/Syllabore/ListExtensions.cs
@@ -11,61 +11,6 @@ namespace Syllabore
     /// </summary>
     public static class ListExtensions
     {
-
-        /// <summary>
-        /// Clears the contents of the current list and replaces it
-        /// with all elements of the new list.
-        /// </summary>
-        public static void ReplaceWith<T>(this IList<T> current, IList<T> newValues)
-        {
-            current.Clear();
-
-            // AddRange doesn't exist for IList<T>
-            foreach(var value in newValues)
-            {
-                current.Add(value);
-            }
-
-        }
-
-        /// <summary>
-        /// Given a list of <see cref="IWeighted"/>, this method returns the sum of
-        /// all elements' <c>Weight</c> property.
-        /// </summary>
-        public static int TotalWeight<T>(this IList<T> weightedItems) where T : IWeighted
-        {
-            return weightedItems.Sum(x => x.Weight);
-        }
-
-        /// <summary>
-        /// Returns a random item from an <see cref="IList{T}"/> using the specified
-        /// instance of <see cref="System.Random"/>.
-        /// </summary>
-        public static T RandomItem<T>(this IList<T> list, Random random)
-        {
-
-            if(list.Count == 0)
-            {
-                return default(T);
-            }
-            else
-            {
-                int selection = random.Next(list.Count);
-
-                return list[selection];
-            }
-
-        }
-
-        /// <summary>
-        /// Returns a random item from an <see cref="ISet{T}"/>
-        /// using the specified instance of <see cref="System.Random"/>
-        /// </summary>
-        public static T RandomItem<T>(this ISet<T> set, Random random)
-        {
-            return set.ToList<T>().RandomItem<T>(random);
-        }
-
         /// <summary>
         /// Returns a weighted-random item from an <see cref="IList{T}"/>, 
         /// where <c>T</c> implements <see cref="IWeighted"/>.
@@ -77,7 +22,7 @@ namespace Syllabore
         /// </summary>
         public static T RandomWeightedItem<T>(this IList<T> weightedItems, Random random) where T : IWeighted
         {
-            int totalWeight = weightedItems.TotalWeight();
+            int totalWeight = weightedItems.Sum(x => x.Weight);
             int randomSelection = random.Next(totalWeight);
 
             int runningTotal = 0;
