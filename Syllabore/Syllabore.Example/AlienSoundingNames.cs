dbf1e84ad9aaee208e5bbdfa455b634ee11f28f8
kesac <kevin@turtlesort.com>
2025-03-18 20:33:33 -0600

    Added two more examples: alien names and boss names

---
+++ Syllabore/Syllabore.Example/AlienSoundingNames.cs
@@ -0,0 +1,106 @@
+﻿using Archigen;
+using Syllabore.Fluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syllabore.Example
+{
+    /// <summary>
+    /// Names that sound like they could be from an alien species.
+    /// </summary>
+    public class AlienSoundingNames : Example
+    {
+        /// <summary>
+        /// Names like: "Xal-Kithath", "Z'nuth", "Yommur"
+        /// </summary>
+        public IGenerator<string> GetGenerator()
+        {
+            var result = new GeneratorPool<string>();
+            result.Add(GetSimpleNameGenerator(2));
+            result.Add(GetApostropheNameGenerator());
+            result.Add(GetDashedNameGenerator());
+
+            return result;
+        }
+
+        /// <summary>
+        /// Names like "Xuul-Tizdu", "Zal-Tathkuth", "Xam-Gidu"
+        /// </summary>
+        public IGenerator<string> GetDashedNameGenerator()
+        {
+            var names = new NameGenerator()
+                .All(x => x
+                    .First(x => x
+                        .Add("ngdkt").Weight(2)
+                        .Cluster("th", "ts", "qu"))
+                    .Middle(x => x
+                        .Add("aiou").Weight(3)
+                        .Add("e"))
+                    .Last(x => x
+                        .Add("z")
+                        .Cluster("th").Weight(2))
+                        .Chance(0.1))
+                .SetSize(2);
+
+            var result = new NameFormatter("{start}-{end}");
+            result.Define("start", GetSimpleNameGenerator(1));
+            result.Define("end", names);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Generates names like: "Za'knuk", "Y'thut", "Xo'tsuuthtsuk"
+        /// </summary>
+        public IGenerator<string> GetApostropheNameGenerator()
+        {
+            var front = new NameGenerator()
+                .All(x => x
+                    .First("kxyz")
+                    .Middle("oai").Chance(0.5))
+                .SetSize(1);
+
+            var back = new NameGenerator()
+                .All(x => x
+                    .First(x => x
+                        .Add("ngd").Weight(2)
+                        .Cluster("th", "ts", "qu", "kn"))
+                    .Middle(x => x
+                        .Add("ua").Weight(4)
+                        .Cluster("uu", "aa"))
+                    .Last(x => x
+                        .Add("rgkt")
+                        .Cluster("th"))
+                        .Chance(0.9))
+                .Filter("gqu")
+                .SetSize(1, 2);
+
+            var result = new NameFormatter("{front}'{back}");
+            result.Define("front", front);
+            result.Define("back", back, NameFormat.LowerCase);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Generates names like: "Xog", "Yalzath", and "Xaathath"
+        /// </summary>
+        public IGenerator<string> GetSimpleNameGenerator(int size)
+        {
+            return new NameGenerator()
+                .All(x => x
+                    .First("xyz").Chance(0.9)
+                    .Middle(x => x
+                        .Add("oai").Weight(2)
+                        .Cluster("uu", "aa"))
+                    .Last(x => x
+                        .Add("lgrm")
+                        .Cluster("th", "gg")))
+                .Filter("^aa", "^uu")
+                .SetSize(size);
+        }
+    }
+}
---
+++ Syllabore/Syllabore.Example/BossNames.cs
@@ -0,0 +1,77 @@
+﻿using Archigen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Syllabore.Fluent;
+
+namespace Syllabore.Example
+{
+    /// <summary>
+    /// Names that sound like they could be a boss in a video game
+    /// </summary>
+    public class BossNames : Example
+    {
+        private IGenerator<string> _names;
+
+        private RandomSelector<string> _adjectives;
+        private RandomSelector<string> _places;
+        private RandomSelector<string> _titles;
+        private RandomSelector<string> _colors;
+        private List<string> _formats;
+
+        public BossNames()
+        {
+            // A simple name for the boss
+            _names = new NameGenerator()
+                .All(x => x
+                    .First("stlr")
+                    .Middle("aeio"))
+                .SetSize(2, 3);
+
+            // Descriptors for the boss
+            _adjectives = new RandomSelector<string>("burned", "ruined", "ascended", "petrified");
+            _places = new RandomSelector<string>("abyss", "void", "city", "sky");
+            _titles = new RandomSelector<string>("sage", "king", "knight", "priest");
+            _colors = new RandomSelector<string>("black", "ashen", "gold", "silver");
+
+            _formats = new List<string>
+            {
+                "{adjective} {title} {name}",
+                "{name} of The {adjective} {place}",
+                "{title} {name}, The One Who {adjective}",
+                "The {adjective} {title}, {name} of The {color} {place}"
+            };
+
+        }
+
+        /// <summary>
+        /// Returns a generator with names like:
+        /// "The Burned Knight, Latula of the Ashen City",
+        /// "Sirita of The Petrified Abyss",
+        /// "Ascended King Ralo"
+        /// </summary>
+        public IGenerator<string> GetGenerator()
+        {
+            var pool = new GeneratorPool<string>();
+
+            foreach(var format in _formats)
+            {
+                pool.Add(GetFormatter(format));
+            }
+
+            return pool;
+        }
+
+        private IGenerator<string> GetFormatter(string format)
+        {
+            return new NameFormatter(format)
+                .Define("name", _names)
+                .Define("adjective", _adjectives)
+                .Define("place", _places)
+                .Define("title", _titles)
+                .Define("color", _colors);
+        }
+    }
+}
